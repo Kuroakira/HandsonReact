ae6a5099abeecce208465a84cdc32217c9511a76
Akira <jinnseitanosi@gmail.com>
2023-07-25 11:59:53 +0900

    Until HTTP

--- keeptrack/src/projects/ProjectsPage.tsx
+++ keeptrack/src/projects/ProjectsPage.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { MOCK_PROJECTS } from './MockProjects';
 import ProjectList from './ProjectList';
+import { Project } from './Project';
 
 function ProjectsPage() {
-  return (
-    <>
-        <h1>Projects</h1>
-        <ProjectList projects={MOCK_PROJECTS} />
-    </>
-  )
+    const saveProject = (project: Project) => {
+        console.log('Saving project:', project);
+    }
+
+    return (
+        <>
+            <h1>Projects</h1>
+            <ProjectList 
+                onSave={saveProject}
+                projects={MOCK_PROJECTS} />
+        </>
+    )
 }
 
 export default ProjectsPage;
--- readme.md
+++ readme.md
@@ -4,3 +4,8 @@ https://handsonreact.com/docs/labs/react-tutorial-typescript
 
 ## Environment Building
 https://qiita.com/dameneko/items/840e65dd1cf29b2001a5
+
+# What I want to keep in mind
+- https://handsonreact.com/docs/component-architecture#thinking-in-react
+- This program is very interesting.
+  - https://handsonreact.com/docs/forms#items-app-demo-crud-continued
