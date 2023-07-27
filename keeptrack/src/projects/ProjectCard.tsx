7f010b07e0c9b1eec2116d3475f7c2f1335833d7
Akira <jinnseitanosi@gmail.com>
2023-07-27 11:48:06 +0900

    Lab21 Done

--- keeptrack/src/projects/ProjectCard.tsx
+++ keeptrack/src/projects/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { Project } from './Project';
 import React from 'react';
 
@@ -19,20 +20,22 @@ function ProjectCard(props: ProjectCardProps) {
         <div className='card'>
             <img src={project.imageUrl} alt={project.name} />
             <section className="section dark">
-                <h5 className='strong'>
-                    <strong>{project.name}</strong>
-                </h5>
-                <p>{formatDescription(project.description)}</p>
-                <p>Budget : {project.budget.toLocaleString()}</p>
-                <button 
-                    className='bordered'
-                    onClick={() => {
-                        handleEditClick(project);
-                    }}
-                >
-                    <span className='icon-edit'></span>
-                    Edit
-                </button>
+                <Link to={'/projects/' + project.id}>
+                    <h5 className='strong'>
+                        <strong>{project.name}</strong>
+                    </h5>
+                    <p>{formatDescription(project.description)}</p>
+                    <p>Budget : {project.budget.toLocaleString()}</p>
+                    <button 
+                        className='bordered'
+                        onClick={() => {
+                            handleEditClick(project);
+                        }}
+                    >
+                        <span className='icon-edit'></span>
+                        Edit
+                    </button>  
+                </Link>
             </section>
         </div>
     );
--- readme.md
+++ readme.md
@@ -9,3 +9,7 @@ https://qiita.com/dameneko/items/840e65dd1cf29b2001a5
 - https://handsonreact.com/docs/component-architecture#thinking-in-react
 - This program is very interesting.
   - https://handsonreact.com/docs/forms#items-app-demo-crud-continued
+
+# I want to try DENO after this tutorial
+- Build Dev container with deno: https://zenn.dev/overflow_offers/articles/20230327-deno-devcontainer-dev-env-setup
+- https://www.youtube.com/watch?v=zU6-8w1IR-I
